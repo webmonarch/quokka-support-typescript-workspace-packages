5902703ed19a7401fe4e451542790caea94c165f
Eric Webb <eric@webb.codes>
2025-05-12 23:08:35 -0700

    some comments

--- packages/a/index.ts
+++ packages/a/index.ts
@@ -4,6 +4,11 @@
     // recycle: false,
 })
 
-import { name } from 'b'
+/**
+ * !!! Run Quokka from here
+ */
+
+// THIS FAILED WITH: TypeError [ERR_UNKNOWN_FILE_EXTENSION]: Unknown file extension ".ts" for /Users/ewebb/Code/quokka-support-typescript-workspace-packages/packages/b/index.ts
+import { name } from 'b' 
 
 console.log(name)
